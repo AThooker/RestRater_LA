043a27815d926bed709305ee549f3773f79f69b1
AThooker <austintrent@icloud.com>
2020-11-07 09:37:27 -0500

    setup index

---
+++ RestaurantRaterLA/Controllers/RestaurantController.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantRaterLA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterLA.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _ctx = new RestaurantDbContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_ctx.Restaurants.ToList());
+        }
+    }
+}
