a38aab51580c74a60d3f5604366b9ac3632733e5
AThooker <austintrent@icloud.com>
2020-11-09 08:43:53 -0500

    full crud for restRater

--- RestaurantRaterLA/Controllers/RestaurantController.cs
+++ RestaurantRaterLA/Controllers/RestaurantController.cs
@@ -1,7 +1,9 @@
 ﻿using RestaurantRaterLA.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -34,6 +36,77 @@ namespace RestaurantRaterLA.Controllers
             _ctx.Restaurants.Add(restaurant);
             _ctx.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //GET: Restaurant Delete
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant rst = _ctx.Restaurants.Find(id);
+            if(rst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rst);
+        }
+
+        //POST: Restaurant Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var rest = _ctx.Restaurants.Find(id);
+            _ctx.Restaurants.Remove(rest);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Restaurant Update
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var rest = _ctx.Restaurants.Find(id);
+            if(rest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rest);
+        }
+
+        //POST: Restaurant Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(restaurant);
+            }
+            _ctx.Entry(restaurant).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Restaurant Details
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var rest = _ctx.Restaurants.Find(id);
+            if(rest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rest);
         }
     }
 }
